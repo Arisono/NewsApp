b6782beacebe296b6cda37988c4427fe4cfd70ee
LiuJie <784602719@qq.com>
2016-01-04 17:55:55 +0800

    toolbar hide for scroller

--- app/src/main/java/com/news/fragment/NewsFragment.java
+++ app/src/main/java/com/news/fragment/NewsFragment.java
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,10 +32,8 @@ public class NewsFragment extends Fragment{
 
     @Bind(R.id.mlist)
     public ListView mlist;
-
     private NewsListAdapter adapter;
     Toolbar mMainToolbar;
-    private float mStartY = 0, mLastY = 0, mLastDeltaY;
 
     @Nullable
     @Override
@@ -59,48 +58,6 @@ public class NewsFragment extends Fragment{
        adapter=new NewsListAdapter(getActivity(),data);
        mlist.setAdapter(adapter);
 
-
-
-       mlist.setOnTouchListener(new View.OnTouchListener() {
-           @Override
-           public boolean onTouch(View v, MotionEvent event) {
-               final float y = event.getY();
-               float translationY = mMainToolbar.getTranslationY();
-               switch (event.getAction()) {
-                   case MotionEvent.ACTION_DOWN:
-//						Log.v(TAG, "Down");
-                       mStartY = y;
-                       mLastY = mStartY;
-                       break;
-                   case MotionEvent.ACTION_MOVE:
-                       float mDeltaY = y - mLastY;
-
-                       float newTansY = translationY + mDeltaY;
-                       if (newTansY <= 0 && newTansY >= -mMainToolbar.getHeight()) {
-                           mMainToolbar.setTranslationY(newTansY);
-                       }
-                       mLastY = y;
-                       mLastDeltaY = mDeltaY;
-//						Log.v(TAG, "Move");
-                       break;
-                   case MotionEvent.ACTION_UP:
-                       ObjectAnimator animator = null;
-                      // Log.d(TAG, "mLastDeltaY=" + mLastDeltaY);
-                       if (mLastDeltaY < 0 && mlist.getFirstVisiblePosition() > 1) {
-                         //  Log.v(TAG, "listView.first=" + mlist.getFirstVisiblePosition());
-                           animator = ObjectAnimator.ofFloat(mMainToolbar, "translationY", mMainToolbar.getTranslationY(), -mMainToolbar.getHeight());
-                       } else {
-                           animator = ObjectAnimator.ofFloat(mMainToolbar, "translationY", mMainToolbar.getTranslationY(), 0);
-                       }
-                       animator.setDuration(100);
-                       animator.start();
-                       animator.setInterpolator(AnimationUtils.loadInterpolator(getActivity(), android.R.interpolator.linear));
-//						Log.v(TAG, "Up");
-                       break;
-               }
-               return false;
-           }
-       });
    }
 
     public class NewsListAdapter extends BaseAdapter{
